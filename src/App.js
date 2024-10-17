3e0e81cf0b43af23c23bd263c623d456eb065927
ashwin-14876 <ashwin.madhu@zohocorp.com>
2024-10-17 14:02:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="card">
+        <div className="texts">
+          <h1>A PROUD TEAM</h1>
+          <p>Together from the very start, we’ve built something special… A PROUD TEAM, united in purpose!</p>
+        </div>
+        <img src="https://blogimage.vantagecircle.com/content/images/2024/01/Employee-Experience-Everything-You-Need-To-Know-in-2024-Featured-Image.png" alt="An Error Occured!"/>
+      </div>
+  );
+}
+
+export default App;
